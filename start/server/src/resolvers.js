e28c97962a511f2c35836c4ac2901daa37e513cb
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-09 07:48:21 +0100

    add user login mutation

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -20,6 +20,13 @@ module.exports = {
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
   },
 
+  Mutation: {
+    login: async (_, {email}, {dataSources}) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) return Buffer.from(email).toString('base64');
+    }
+  },
+
   Mission: {
     missionPatch: (mission, { size } = { size: 'LARGE' }) => {
       return size === 'SMALL' ? missionPatchSmall : missionPatchLarge;
