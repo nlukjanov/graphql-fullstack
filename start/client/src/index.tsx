a0069dad1ac384140af23d04cceecf0540739b62
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-10 13:11:17 +0100

    integrate with React

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,26 +1,24 @@
 import {
   ApolloClient,
   InMemoryCache,
-  gql,
-  NormalizedCacheObject
+  NormalizedCacheObject,
+  ApolloProvider
 } from '@apollo/client';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import injectStyles from './styles';
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache()
 });
 
-client
-  .query({
-    query: gql`
-      query TestQuery {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `
-  })
-  .then((result) => console.log(result));
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
