b6bc7c80f26d24f7ed1f92ee3eec1641fdd49756
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-08-10 08:16:45 +0100

    add bookTrips and cancelTrip mutations

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -6,14 +6,19 @@ module.exports = {
       const allLaunches = await dataSources.launchAPI.getAllLaunches();
       allLaunches.reverse();
       const launches = paginateResults({
-        after, pageSize, results: allLaunches
-      })
+        after,
+        pageSize,
+        results: allLaunches
+      });
 
       return {
         launches,
         cursor: launches.length ? launches[launches.length - 1].cursor : null,
-        hasMore: launches.length ? launches[launches.length - 1].cursor !== allLaunches[allLaunches.length - 1].cursor : false
-      }
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length - 1].cursor
+          : false
+      };
     },
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
@@ -21,9 +26,37 @@ module.exports = {
   },
 
   Mutation: {
-    login: async (_, {email}, {dataSources}) => {
+    login: async (_, { email }, { dataSources }) => {
       const user = await dataSources.userAPI.findOrCreateUser({ email });
       if (user) return Buffer.from(email).toString('base64');
+    },
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds
+      });
+      return {
+        success: results && results.length === launchIds.length,
+        message:
+          results.length === launchIds.length
+            ? 'trips booked successfully'
+            : `the following cannot be booked: ${launchIds.filter(
+                (id) => !results.includes(id)
+              )}`,
+        launches
+      };
+    },
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = await dataSources.userAPI.cancelTrip({ launchId });
+
+      if (!result) return { success: false, message: 'failed to cancel trip' };
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'trip cancelled',
+        launches: [launch]
+      };
     }
   },
 
